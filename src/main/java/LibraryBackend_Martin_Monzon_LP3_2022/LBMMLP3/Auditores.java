2e698023a156b1acc2cb1583eb97c411e74c8df2
C4russian <martinmonzon00@gmail.com>
2022-11-19 01:15:12 -0300

    Org hierarchy updt

---
+++ src/main/java/LibraryBackend_Martin_Monzon_LP3_2022/LBMMLP3/Auditores.java
@@ -0,0 +1,5 @@
+package LibraryBackend_Martin_Monzon_LP3_2022.LBMMLP3;
+
+public class Auditors extends Organization {
+
+}
---
+++ src/main/java/LibraryBackend_Martin_Monzon_LP3_2022/LBMMLP3/Customsagency.java
@@ -0,0 +1,5 @@
+package LibraryBackend_Martin_Monzon_LP3_2022.LBMMLP3;
+
+public class Customsagency extends Organization {
+	public String description;
+}
