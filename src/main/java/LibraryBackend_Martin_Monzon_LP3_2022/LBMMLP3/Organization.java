ef511ab9aa01357290f3bfb10cbdfd0bb7030347
C4russian <martinmonzon00@gmail.com>
2022-11-19 01:08:06 -0300

    update org class

--- src/main/java/LibraryBackend_Martin_Monzon_LP3_2022/LBMMLP3/Organization.java
+++ src/main/java/LibraryBackend_Martin_Monzon_LP3_2022/LBMMLP3/Organization.java
@@ -3,6 +3,7 @@ package LibraryBackend_Martin_Monzon_LP3_2022.LBMMLP3;
 public class Organization {
 	public String ID;
 	public String name;
-	public Services[] services;
+	public Service[] services;
 	protected User[] users;
+	public String description;
 }
